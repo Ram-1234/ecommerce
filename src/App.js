6d749d1c9a81a15ca5f05c583a15b295364997be
Ram-1234 <ramnayan699@gmail.com>
2021-05-12 16:19:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import './App.css';
 import {BrowserRouter as Router, Route,Switch}from "react-router-dom";
 import Home from './components/Home';
@@ -10,7 +10,29 @@ import Signup from './components/Signup';
 
 
 function App() {
+
+  // useEffect(() => {
+  //   //will only run once when the app component loads...
   
+  //   auth.onAuthStateChanged((authUser) => {
+  //     console.log("THE USER IS >>> ", authUser);
+  
+  //     if (authUser) {
+  //       // the user just logged in / the user was logged in
+  
+  //       dispatch({
+  //         type: "SET_USER",
+  //         user: authUser,
+  //       });
+  //     } else {
+  //       // the user is logged out
+  //       dispatch({
+  //         type: "SET_USER",
+  //         user: null,
+  //       });
+  //     }
+  //   });
+  // }, []);
 
   return (
     <div className="App">
